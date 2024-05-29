8e5669989840f0cecc822dd5b4f8efaf72b44aa9
tanishqsoni0309 <tanishqsoni0309@gmail.com>
2024-05-29 13:58:02 +0530

    search functionality added + all bugs fixed

--- src/components/blogs/blogNeeds/BlogContainer.jsx
+++ src/components/blogs/blogNeeds/BlogContainer.jsx
@@ -5,31 +5,33 @@ import Link from 'next/link';
 function BlogContainer({id,title,description,category,img,authorDetails}) {
   return (
     <div className="mt-4">
-    <div className="bg-white shadow-lg rounded-lg max-w-sm mb-5 p-6">
-        <a href="#">
-            <img className="" src={img} alt=""/>
-        </a>
-        <div className="mt-2">
-            <a href="#" className=''>
-                <span className="text-purple-700 font-medium normal-case">{category}</span>
-            </a>
-            <Link href={`/blog/${id}`} className='flex justify-between'>
-                <h5 className="text-gray-900 font-semibold text-2xl mb-2">{title}</h5>
-                <ArrowOutwardIcon/>
-            </Link>
-            <p className="font-normal text-gray-700 mb-3">{description}</p>
-            
-            <div className="flex mt-12 items-center">
-                <div className="w-1/6 p-1">
-                    <img className='w-full rounded-full' src={authorDetails.img} alt="" />
+        <div className="bg-white shadow-lg rounded-lg max-w-sm mb-5 p-6">
+            <Link href={`/blog/${id}`}>
+                <div className="w-full h-64 overflow-hidden">
+                    <img className="w-full h-full object-cover" src={img} alt=""/>
                 </div>
-                <div className="ml-2 w-5/6">
-                    <a href="#" className="text-gray-700 font-medium">{authorDetails.name}</a>
-                    <p className="text-gray-500 font-normal">{authorDetails.date}</p>
+            </Link>
+            <div className="mt-2">
+                <a href="#" className=''>
+                    <span className="text-purple-700 font-medium normal-case">{category}</span>
+                </a>
+                <Link href={`/blog/${id}`} className='flex justify-between'>
+                    <h5 className="text-gray-900 font-semibold text-2xl mb-2">{title}</h5>
+                    <ArrowOutwardIcon/>
+                </Link>
+                <p className="font-normal text-gray-700 mb-3">{description}... <Link className='text-theme font-semibold' href={`/blog/${id}`}>Read more</Link></p>
+                <div className="flex mt-8 items-center">
+                    <div className="w-12 h-12 overflow-hidden rounded-full p-1">
+                        <img className='w-full h-full object-cover rounded-full' src={authorDetails.img} alt="" />
+                    </div>
+                    <div className="ml-2 w-5/6">
+                        <a href="#" className="text-gray-700 font-medium">{authorDetails.name}</a>
+                        <p className="text-gray-500 font-normal">{authorDetails.date}</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div></div>  
+    </div>  
   )
 }
 
