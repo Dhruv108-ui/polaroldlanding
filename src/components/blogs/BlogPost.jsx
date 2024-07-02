c5c2702266fd68921ea6870b443757d8bfb19f70
Princerey <rohkumar0126@gmail.com>
2024-07-02 16:19:14 +0530

    Blog switched to server side

--- src/components/blogs/BlogPost.jsx
+++ src/components/blogs/BlogPost.jsx
@@ -1,50 +1,11 @@
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import { ArrowBack, ThumbDown, ThumbUp } from '@mui/icons-material';
+import React, { useState } from 'react';
+import { ArrowBack } from '@mui/icons-material';
 import CircleIcon from '@mui/icons-material/Circle';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import facebookSvg from '@/assets/blogs/facebook.svg';
-import Image from 'next/image';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import BlogContainer from './blogNeeds/BlogContainer';
-import ReplyIcon from '@mui/icons-material/Reply';
-import CloseIcon from '@mui/icons-material/Close';
-import { database } from '@/assets/db/config';
-import { getDocs, collection } from 'firebase/firestore';
 import Head from 'next/head';
+import Link from 'next/link';
 
-// Dynamic Metadata Generation
-export async function generateMetadata({ params }) {
-  const { slug } = params;
-  const blogCollection = collection(database, 'blog');
-  const blogSnapshot = await getDocs(blogCollection);
-  const blogList = blogSnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  const blogData = blogList.find((blog) => blog.id === slug);
-
-  return {
-    title: blogData.title,
-    description: blogData.description,
-    openGraph: {
-      title: blogData.title,
-      description: blogData.description,
-      images: [blogData.thumbnail],
-    },
-    twitter: {
-      title: blogData.title,
-      description: blogData.description,
-      image: blogData.thumbnail,
-      card: 'summary_large_image',
-    },
-  };
-}
-
-function BlogPost({ slug }) {
-  const [blog, setBlog] = useState({});
-  const [timeToRead, setTimeToRead] = useState(0);
-  const [blogs, setBlogs] = useState([]);
+const BlogPost = ({ blog, relatedBlogs }) => {
   const [like, setLike] = useState(false);
   const [replyOpenState, setReplyState] = useState({
     index: 0,
@@ -58,66 +19,11 @@ function BlogPost({ slug }) {
     state: false,
   });
 
-  function timeToReadBlog(content) {
+  const timeToReadBlog = (content) => {
     const wordsPerMinute = 200;
     const noOfWords = content.split(/\s/g).length;
     return Math.ceil(noOfWords / wordsPerMinute);
-  }
-
-  useEffect(() => {
-    if (!slug) return;
-
-    async function getBlogDetails() {
-      try {
-        const blogCollection = collection(database, 'blog');
-        const blogSnapshot = await getDocs(blogCollection);
-        const blogList = blogSnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        const blogData = blogList.find((blog) => blog.id === slug);
-        if (blogData) {
-          setBlog(blogData);
-          setTimeToRead(timeToReadBlog(blogData.description));
-          document.getElementById('blog_description').innerHTML =
-            blogData.description;
-        } else {
-          console.log('No Blog Found');
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    async function getRelatedBlogs() {
-      try {
-        const blogCollection = collection(database, 'blog');
-        const blogSnapshot = await getDocs(blogCollection);
-        const blogList = blogSnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        const relatedBlogs = blogList.filter(
-          (blog) => blog.category === blog.category && blog.id !== slug
-        );
-        relatedBlogs.forEach((blog) => {
-          blog.description = blog.description.substring(0, 100);
-          blog.description =
-            blog.description + `...<a href='/blog/${blog.id}'>Read More</a>`;
-        });
-        if (relatedBlogs.length > 3) {
-          setBlogs(relatedBlogs.slice(0, 3));
-        } else {
-          setBlogs(relatedBlogs);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    getBlogDetails();
-    getRelatedBlogs();
-  }, [slug]);
+  };
 
   const handleLike = () => {
     setLike(true);
@@ -166,50 +72,44 @@ function BlogPost({ slug }) {
           <ArrowBack sx={{ width: '2rem', height: '2rem' }} />
         </Link>
       </div>
-      <h1 className="text-6xl md:w-4/6 sm:w-full xs:w-full mt-10">
-        {blog.title}
-      </h1>
+      <h1 className="text-6xl md:w-4/6 sm:w-full xs:w-full mt-10">{blog.title}</h1>
       <div className="border-y border-black w-full mt-10 flex px-8 py-8 justify-between">
         <div className="date flex items-center text-xl">
           <span className="mr-2">{blog.createdAt}</span>
           <CircleIcon sx={{ fontSize: '0.5rem' }} />
-          <span className="ml-2">{timeToRead} min read</span>
+          <span className="ml-2">{timeToReadBlog(blog.description)} min read</span>
         </div>
       </div>
       <div className="banner mt-12">
         <img className="w-full" src={blog.thumbnail} alt="" />
       </div>
-      <div className="blog-content p-6 text-lg" id="blog_description" />
+      <div className="blog-content p-6 text-lg" id="blog_description" dangerouslySetInnerHTML={{ __html: blog.description }} />
       <div className="w-full border border-gray-400 mt-8"></div>
 
       <h1 className="text-4xl mt-12">Related Blogs</h1>
       <div className="flex mt-2 flex-wrap justify-between mb-8">
-        {blogs &&
-          blogs.map((blog) => (
+        {relatedBlogs &&
+          relatedBlogs.map((relatedBlog) => (
             <BlogContainer
-              key={blog.id}
-              id={blog.id}
-              title={blog.title}
-              description={
-                blog.description +
-                `...<a class="text-theme font-semibold" href='/blog/${blog.id}'>Read More</a>`
-              }
-              category={blog.category}
-              img={blog.thumbnail}
+              key={relatedBlog.id}
+              id={relatedBlog.id}
+              title={relatedBlog.title}
+              description={relatedBlog.description}
+              category={relatedBlog.category}
+              img={relatedBlog.thumbnail}
               authorDetails={{
-                name: blog.authorName,
-                date: blog.createdAt,
-                img: blog.authorImage,
+                name: relatedBlog.authorName,
+                date: relatedBlog.createdAt,
+                img: relatedBlog.authorImage,
               }}
             />
           ))}
-        {blogs && blogs.length === 0 && (
+        {relatedBlogs && relatedBlogs.length === 0 && (
           <h1 className="text-center w-full text-lg">No Related Blogs Found</h1>
         )}
       </div>
     </>
   );
-}
+};
 
 export default BlogPost;
-
--- src/pages/blog/[blogId].js
+++ src/pages/blog/[blogId].js
@@ -1,13 +1,56 @@
-import BlogPost from '@/components/blogs/BlogPost'
-import { useRouter } from 'next/router'
- 
-export default function Page() {
-  const router = useRouter()
+import BlogPost from '@/components/blogs/BlogPost';
+import { collection, getDocs } from 'firebase/firestore';
+import { database } from '@/assets/db/config';
+
+export default function Page({ blog, relatedBlogs }) {
   return (
-    <>
-        <div className="container mx-auto">
-            <BlogPost slug={router.query.blogId} />
-        </div>
-    </>
-  )
+    <div className="container mx-auto">
+      <BlogPost blog={blog} relatedBlogs={relatedBlogs} />
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { blogId } = context.params;
+
+  try {
+    const blogCollection = collection(database, 'blog');
+    const blogSnapshot = await getDocs(blogCollection);
+    const blogList = blogSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    const blog = blogList.find((blog) => blog.id === blogId);
+
+    if (!blog) {
+      return {
+        notFound: true,
+      };
+    }
+
+    const relatedBlogs = blogList.filter(
+      (relatedBlog) => relatedBlog.category === blog.category && relatedBlog.id !== blogId
+    ).map((relatedBlog) => ({
+      ...relatedBlog,
+      description: relatedBlog.description.substring(0, 100) + '...<a href="/blog/' + relatedBlog.id + '">Read More</a>',
+    }));
+    const allBogs = blogList.filter(
+      (relatedBlog) => relatedBlog.category === relatedBlog.category && relatedBlog.id !== blogId
+    ).map((relatedBlog) => ({
+      ...relatedBlog,
+      description: relatedBlog.description.substring(0, 100) + '...<a href="/blog/' + relatedBlog.id + '">Read More</a>',
+    }));
+
+    return {
+      props: {
+        blog,
+        relatedBlogs: relatedBlogs.length > 0 ? relatedBlogs.slice(0, 3) : allBogs.slice(0, 3)
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching blog data:', error);
+    return {
+      notFound: true,
+    };
+  }
 }
